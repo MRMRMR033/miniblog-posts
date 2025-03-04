72bebf732b10e4f1454d446e2289ec9c51934545
MRMRMR033 <crabs.rust@icloud.com>
2025-03-03 22:02:37 -0600

    service de role

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
+import { RoleGuard } from 'src/guards/role.guard';
 
+@UseGuards(RoleGuard)
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
@@ -27,8 +29,4 @@ export class AdminController {
     return this.adminService.update(+id, updateAdminDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.adminService.remove(+id);
-  }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,26 +1,53 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
-
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from "bcrypt";
+import { User } from 'src/user/entities/user.entity';
 @Injectable()
 export class AdminService {
-  create(createAdminDto: CreateAdminDto) {
-    return 'This action adds a new admin';
+  constructor(private prismaService: PrismaService) {}
+  async create(createAdminDto: CreateAdminDto) {
+    try{
+      const hashedPassword = await bcrypt.hash(createAdminDto.password, 10)
+      return await this.prismaService.user.create({data: {
+        name: createAdminDto.name,
+        email: createAdminDto.email,
+        password: hashedPassword,
+        role: createAdminDto.role
+      }});
+    }
+    catch{
+      console.log('error al crear el admin')
+      throw new Error('Error al crear el admin')
+    }
   }
 
-  findAll() {
-    return `This action returns all admin`;
+  async findAll(): Promise<User[]> {
+    try {
+      return await this.prismaService.user.findMany({where: {role: "ADMIN"}})
+    } catch (error) {
+      console.log('error al obtener todos los admin')
+      throw new Error('Error al obtener todos los admin')
+    }
+  
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} admin`;
+  async findOne(id: number) {
+    try {
+      return await this.prismaService.user.findUnique({where: {id}})
+    } catch (error) {
+      console.log('error al obtener el admin')
+      throw new Error('Error al obtener el admin')
+    }
   }
 
-  update(id: number, updateAdminDto: UpdateAdminDto) {
-    return `This action updates a #${id} admin`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} admin`;
+  async update(id: number, updateAdminDto: UpdateAdminDto) {
+    try {
+      return await this.prismaService.user.update({where: {id}, data: updateAdminDto})
+    } catch (error) {
+      console.log('error al actualizar el admin')
+      throw new Error('Error al actualizar el admin')
+    }
   }
 }
